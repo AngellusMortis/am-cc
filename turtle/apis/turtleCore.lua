bebc1cc1c1ae4c90aa88562f45b1e7635412f9d6
Christopher Bailey <cbailey@mort.is>
2022-04-27 23:33:16 -0400

    Refactor turtleCore

--- turtle/apis/turtleCore.lua
+++ turtle/apis/turtleCore.lua
@@ -8,6 +8,176 @@ local pathfind = require("pathfind")
 local eventLib = require("eventLib")
 
 local turtleCore = {}
+local moveDirForward = 1
+local moveDirDown = 2
+local moveDirUp = 3
+
+local fillBlocks = {
+    "minecraft:cobblestone",
+    "minecraft:dirt",
+    "minecraft:andesite",
+    "minecraft:diorite",
+    "minecraft:granite",
+}
+
+local function dirStr(moveDir)
+    v.expect(1, moveDir, "number")
+    v.range(moveDir, 1, 3)
+
+    if moveDir == moveDirForward then
+        return ""
+    elseif moveDir == moveDirDown then
+        return " Down"
+    else
+        return " Up"
+    end
+end
+
+local function isSourceBlock(data)
+    return data[1] and data[2].state.level == 0
+end
+
+local function isFillBlock(block)
+    if block == nil then
+        return false
+    end
+end
+
+local function selectFill()
+    while true do
+        for i = 1, 16, 1 do
+            local item = turtle.getItemDetail(i)
+            if isFillBlock(turtle.getItemDetail(i)) then
+                turtle.select(i)
+                return
+            end
+        end
+        turtleCore.error("Need Fill Block")
+        sleep(5)
+    end
+end
+
+local function digDir(moveDir)
+    v.expect(1, moveDir, "number")
+    v.range(moveDir, 1, 3)
+
+    if moveDir == moveDirForward then
+        return turtle.dig()
+    elseif moveDir == moveDirDown then
+        return turtle.digDown()
+    else
+        return turtle.digUp()
+    end
+end
+
+local function placeDir(moveDir)
+    v.expect(1, moveDir, "number")
+    v.range(moveDir, 1, 3)
+
+    if moveDir == moveDirForward then
+        return turtle.place()
+    elseif moveDir == moveDirDown then
+        return turtle.placeDown()
+    else
+        return turtle.placeUp()
+    end
+end
+
+local function dropDir(moveDir, count)
+    if count == nil then
+        count = 1
+    end
+    v.expect(1, moveDir, "number")
+    v.expect(2, count, "number")
+    v.range(moveDir, 1, 3)
+
+    if moveDir == moveDirForward then
+        return turtle.drop(count)
+    elseif moveDir == moveDirDown then
+        return turtle.dropDown(count)
+    else
+        return turtle.dropUp(count)
+    end
+end
+
+local function suckDir(moveDir, count)
+    if count == nil then
+        count = 1
+    end
+    v.expect(1, moveDir, "number")
+    v.expect(2, count, "number")
+    v.range(moveDir, 1, 3)
+
+    if moveDir == moveDirForward then
+        return turtle.suck(count)
+    elseif moveDir == moveDirDown then
+        return turtle.suckDown(count)
+    else
+        return turtle.suckUp(count)
+    end
+end
+
+local function digEventDir(moveDir, count)
+    if count == nil then
+        count = 1
+    end
+    v.expect(1, moveDir, "number")
+    v.expect(2, count, "number")
+    v.range(moveDir, 1, 3)
+
+    if moveDir == moveDirForward then
+        eventLib.b.turtleDigForward(count)
+    elseif moveDir == moveDirDown then
+        eventLib.b.turtleDigDown(count)
+    else
+        eventLib.b.turtleDigUp(count)
+    end
+end
+
+local function detectDir(moveDir)
+    v.expect(1, moveDir, "number")
+
+    if moveDir == moveDirForward then
+        return turtle.detect()
+    elseif moveDir == moveDirDown then
+        return turtle.detectDown()
+    else
+        return turtle.detectUp()
+    end
+end
+
+local function inspectDir(moveDir)
+    v.expect(1, moveDir, "number")
+
+    if moveDir == moveDirForward then
+        return turtle.inspect()
+    elseif moveDir == moveDirDown then
+        return turtle.inspectDown()
+    else
+        return turtle.inspectUp()
+    end
+end
+
+local function goDir(moveDir)
+    v.expect(1, moveDir, "number")
+
+    if moveDir == moveDirForward then
+        return pathfind.forward()
+    elseif moveDir == moveDirDown then
+        return pathfind.down()
+    else
+        return pathfind.up()
+    end
+end
+
+local function isChestDir(moveDir)
+    local success, data = inspectDir(moveDir)
+    if not success then
+        return false
+    end
+
+    return data.tags["forge:chests"]
+end
 
 turtleCore.hasRequiredFuel = function(count)
     v.expect(1, count, "number")
@@ -62,14 +232,14 @@ turtleCore.refuel = function(count)
     v.range(count, 1)
 
     local needed = count - turtle.getFuelLevel()
-    local chestMsg = "Missing Refuel Chest Below"
+    local chestMsg = "Missing Refuel Chest Above"
     while count > turtle.getFuelLevel() do
-        turtleCore.pullDown(nil, string.format("Need %d More Fuel", needed), chestMsg)
+        turtleCore.pullUp(nil, string.format("Need %d More Fuel", needed), chestMsg)
         turtle.refuel()
 
         needed = count - turtle.getFuelLevel()
     end
-    turtleCore.dropDown()
+    turtle.dropUp()
     return true
 end
 
@@ -108,36 +278,66 @@ turtleCore.hasRoom = function()
     return turtleCore.emptySlots() >= 4
 end
 
-turtleCore.digForward = function(count)
+local function fillDir(moveDir)
+    selectFill()
+    placeDir(moveDir)
+    turtle.select(1)
+end
+
+turtleCore.fillForward = function()
+    fillDir(moveDirForward)
+end
+
+turtleCore.fillDown = function()
+    fillDir(moveDirDown)
+end
+
+turtleCore.fillUp = function()
+    fillDir(moveDirUp)
+end
+
+local function digMoveDir(moveDir, count)
     if count == nil then
         count = 1
     end
-    v.expect(1, count, "number")
-    eventLib.b.turtleDigForward(count)
+    v.expect(1, moveDir, "number")
+    v.expect(2, count, "number")
+    v.range(moveDir, 1, 3)
 
+    digEventDir(moveDir, count)
+    local hasFilled = false
     for i = 1, count, 1 do
         success = false
         while not success do
             local error = false
-            if turtle.detect() then
+            if detectDir(moveDir) then
                 if not turtleCore.hasRoom() then
                     error = true
                     turtleCore.emptyInventoryAndReturn()
                 end
-                if not error and turtleCore.isChest() then
+                if not error and isChestDir(moveDir) then
                     error = true
-                    turtleCore.error("Cannot Dig Chest")
+                    turtleCore.error("Cannot Dig Chest" .. dirStr(moveDir))
                     sleep(5)
                 end
-                if not error and not turtle.dig() then
+                if not error and not digDir(moveDir) then
                     error = true
-                    turtleCore.error("Cannot Dig Block")
+                    turtleCore.error("Cannot Dig Block" .. dirStr(moveDir))
                     sleep(1)
                 end
-            end
-            if not error and not pathfind.forward() then
+            elseif isSourceBlock(inspectDir(moveDir)) then
                 error = true
-                turtleCore.error("Cannot Move")
+                if hasFilled then
+                    turtleCore.error("Cannot Remove Source Block" .. dirStr(moveDir))
+                    sleep(3)
+                else
+                    hasFilled = true
+                    fillDir(moveDir)
+                end
+            end
+            if not error and not goDir(moveDir) then
+                error = true
+                turtleCore.error("Cannot Move" .. dirStr(moveDir))
                 sleep(1)
             end
 
@@ -146,227 +346,80 @@ turtleCore.digForward = function(count)
             end
         end
     end
+end
+
+turtleCore.digForward = function(count)
+    digMoveDir(moveDirForward, count)
 end
 
 turtleCore.digDown = function(count)
-    if count == nil then
-        count = 1
-    end
-    v.expect(1, count, "number")
-    eventLib.b.turtleDigDown(count)
-
-    for i = 1, count, 1 do
-        success = false
-        while not success do
-            local error = false
-            if turtle.detectDown() then
-                if not turtleCore.hasRoom() then
-                    error = true
-                    turtleCore.emptyInventoryAndReturn()
-                end
-                if not error and turtleCore.isChestDown() then
-                    error = true
-                    turtleCore.error("Cannot Dig Chest Below")
-                    sleep(5)
-                end
-                if not error and not turtle.digDown() then
-                    error = true
-                    turtleCore.error("Cannot Dig Block Below")
-                    sleep(1)
-                end
-            end
-            if not error and not pathfind.down() then
-                error = true
-                turtleCore.error("Cannot Move Down")
-                sleep(1)
-            end
-
-            if not error then
-                success = true
-            end
-        end
-    end
+    digMoveDir(moveDirDown, count)
 end
 
 turtleCore.digUp = function(count)
-    if count == nil then
-        count = 1
-    end
-    v.expect(1, count, "number")
-    eventLib.b.turtleDigUp(count)
-
-    for i = 1, count, 1 do
-        success = false
-        while not success do
-            local error = false
-            if turtle.detectUp() then
-                if not turtleCore.hasRoom() then
-                    error = true
-                    turtleCore.emptyInventoryAndReturn()
-                end
-                if not error and turtleCore.isChestUp() then
-                    error = true
-                    turtleCore.error("Cannot Dig Chest Above")
-                    sleep(5)
-                end
-                if not error and not turtle.digUp() then
-                    error = true
-                    turtleCore.error("Cannot Dig Block Above")
-                    sleep(1)
-                end
-            end
-            if not error and not pathfind.up() then
-                error = true
-                turtleCore.error("Cannot Move Up")
-                sleep(1)
-            end
-
-            if not error then
-                success = true
-            end
-        end
-    end
+    digMoveDir(moveDirUp, count)
 end
 
-turtleCore.isChest = function()
-    local success, data = turtle.inspect()
-    if not success then
-        return false
+local function insertDir(moveDir, count, msg, chestMsg)
+    if msg == nil then
+        msg = "Failed to Insert Item" .. dirStr(moveDir)
+    end
+    if chestMsg == nil then
+        chestMsg = "No Chest For Insert" .. dirStr(moveDir)
     end
 
-    return data.tags["forge:chests"]
-end
-
-turtleCore.isChestDown = function()
-    local success, data = turtle.inspectDown()
-    if not success then
-        return false
+    while not isChestDir(moveDir) do
+        turtleCore.error(chestMsg)
+        sleep(5)
     end
 
-    return data.tags["forge:chests"]
-end
-
-turtleCore.isChestUp = function()
-    local success, data = turtle.inspectUp()
-    if not success then
-        return false
+    while not dropDir(moveDir, count) do
+        turtleCore.error(msg)
+        sleep(5)
     end
-
-    return data.tags["forge:chests"]
 end
 
 turtleCore.insert = function(count, msg, chestMsg)
-    if msg == nil then
-        msg = "Failed to Insert Item"
-    end
-    if chestMsg == nil then
-        chestMsg = "No Chest For Insert"
-    end
-
-    while not turtleCore.isChest() do
-        turtleCore.error(chestMsg)
-        sleep(5)
-    end
-
-    while not turtle.drop(count) do
-        turtleCore.error(msg)
-        sleep(5)
-    end
+    insertDir(moveDirForward, count, msg, chestMsg)
 end
 
 turtleCore.insertDown = function(count, msg, chestMsg)
-    if msg == nil then
-        msg = "Failed to Insert Item Below"
-    end
-    if chestMsg == nil then
-        chestMsg = "No Chest For Insert Below"
-    end
-
-    while not turtleCore.isChestDown() do
-        turtleCore.error(chestMsg)
-        sleep(5)
-    end
-
-    while not turtle.dropDown(count) do
-        turtleCore.error(msg)
-        sleep(5)
-    end
+    insertDir(moveDirDown, count, msg, chestMsg)
 end
 
 turtleCore.insertUp = function(count, msg, chestMsg)
+    insertDir(moveDirUp, count, msg, chestMsg)
+end
+
+local function pullDir(moveDir, count, msg, chestMsg)
     if msg == nil then
-        msg = "Failed to Insert Item Above"
+        msg = "Failed to Pull Item" .. dirStr(moveDir)
     end
     if chestMsg == nil then
-        chestMsg = "No Chest For Insert Above"
+        chestMsg = "No Chest For Pull" .. dirStr(moveDir)
     end
 
-    while not turtleCore.isChestUp() do
+    while not isChestDir(moveDir) do
         turtleCore.error(chestMsg)
         sleep(5)
     end
 
-    while not turtle.dropUp(count) do
+    while not suckDir(moveDir, count) do
         turtleCore.error(msg)
         sleep(5)
     end
 end
 
 turtleCore.pull = function(count, msg, chestMsg)
-    if msg == nil then
-        msg = "Failed to Pull Item"
-    end
-    if chestMsg == nil then
-        chestMsg = "No Chest For Pull"
-    end
-
-    while not turtleCore.isChest() do
-        turtleCore.error(chestMsg)
-        sleep(5)
-    end
-
-    while not turtle.suck(count) do
-        turtleCore.error(msg)
-        sleep(5)
-    end
+    pullDir(moveDirForward, count, msg, chestMsg)
 end
 
 turtleCore.pullDown = function(count, msg, chestMsg)
-    if msg == nil then
-        msg = "Failed to Pull Below"
-    end
-    if chestMsg == nil then
-        chestMsg = "No Chest For Pull Below"
-    end
-
-    while not turtleCore.isChestDown() do
-        turtleCore.error(chestMsg)
-        sleep(5)
-    end
-
-    while not turtle.suckDown(count) do
-        turtleCore.error(msg)
-        sleep(5)
-    end
+    pullDir(moveDirDown, count, msg, chestMsg)
 end
 
 turtleCore.pullUp = function(count, msg, chestMsg)
-    if msg == nil then
-        msg = "Failed to Pull Above"
-    end
-    if chestMsg == nil then
-        chestMsg = "No Chest For Pull Above"
-    end
-
-    while not turtleCore.isChestUp() do
-        turtleCore.error(chestMsg)
-        sleep(5)
-    end
-
-    while not turtle.suckUp(count) do
-        turtleCore.error(msg)
-        sleep(5)
-    end
+    pullDir(moveDirUp, count, msg, chestMsg)
 end
 
 return turtleCore
