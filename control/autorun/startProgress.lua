764b8e0b3dd4c78031e299a5991840dadd87fd88
Christopher Bailey <cbailey@mort.is>
2022-05-09 21:03:00 -0400

    Adds check for running progress before starting another

--- control/autorun/startProgress.lua
+++ control/autorun/startProgress.lua
@@ -1,1 +1,17 @@
-shell.run("bg progress")
+---@return boolean
+local function isRunning()
+    local count = 0
+    for i = 1, multishell.getCount(), 1 do
+        if multishell.getTitle(i) == "progress" then
+            count = count + 1
+            if count > 0 then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+if not isRunning() then
+    shell.run("bg progress")
+end
